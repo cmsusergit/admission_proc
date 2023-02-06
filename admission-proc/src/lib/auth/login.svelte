c48a2cd2209d07a6fc53f2805eb01da906cc5e83
cmsusergit <cmsusergit@gmail.com>
2023-02-06 20:22:07 +0530

    auth login

--- admission-proc/src/lib/auth/login.svelte
+++ admission-proc/src/lib/auth/login.svelte
@@ -1,9 +1,25 @@
 <script></script>
 
 
-<div class="mx-auto flex text-center md:flex-row flex-col">
-    <div class="w-1/2 h-screen bg-blue-400"></div>
-    <div class="w-1/2">
-        <h1 class="text-4xl">Login</h1>
+<div class="m-2 px-2">
+    <div class="font-bold  text-lg flex items-center bg-blue-500 rounded shadow shadow-slate-500 text-white py-2 text-center">
+        <img class="w-24 rounded" src="logo.svg" alt="logo">
+        <div class="w-full text-center">
+            <h1 class="lg:text-2xl text-xl">Sardar Vallbhbhai Patel Institute of Technlogy,Vasad.</h1>
+            <h2 class="text-xl font-bold">Provisional Admission Form</h2>
+        </div>
+    </div>
+
+
+    <div class="mx-auto mt-4 flex text-center flex-row ">
+        <div class="w-1/2 md:flex hidden h-screen  mr-2 bg-blue-500 rounded shadow shadow-slate-500 text-white">
+        
+            <div></div>
+            <div class="w-20 h-20 p-2 bg-white rounded-md animate-spin"></div>
+        </div>
+        
+        <div class="w-full md:w-1/2 h-screen bg-slate-200 shadow shadow-slate-500 rounded ">
+            <h1 class="py-4 border bg-slate-500 rounded text-4xl text-white">Login</h1>
+        </div>
     </div>
 </div>
