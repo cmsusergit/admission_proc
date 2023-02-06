d05a19319c09bacc6f5bba9a4da6ef516b437d7b
cmsusergit <cmsusergit@gmail.com>
2023-02-06 20:01:06 +0530

    with login

---
+++ admission-proc/src/lib/auth/login.svelte
@@ -0,0 +1,9 @@
+<script></script>
+
+
+<div class="mx-auto flex text-center md:flex-row flex-col">
+    <div class="w-1/2 h-screen bg-blue-400"></div>
+    <div class="w-1/2">
+        <h1 class="text-4xl">Login</h1>
+    </div>
+</div>
--- admission-proc/src/routes/+layout.svelte
+++ admission-proc/src/routes/+layout.svelte
@@ -1,22 +1,13 @@
 <script>
     import "../app.css"
 
+	import Auth from '$lib/auth/login.svelte'
 </script>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Lato&family=Poppins&display=swap" rel="stylesheet">
 
-    <title>Admission Test</title>
-</head>
-<style> @import url('https://fonts.googleapis.com/css2?family=Lato&family=Poppins&display=swap'); </style>
-<body style="font-family: lato;font-size: 100%;">
-    <div class="container mx-auto w-11/12 min-h-screen text-blue-800">
-        <slot></slot>
-    </div>
-</body>
-</html>
+<div class="container mx-auto w-11/12 min-h-screen text-blue-800">
+    <Auth/>
+    <slot></slot>
+</div>
+
+
+
