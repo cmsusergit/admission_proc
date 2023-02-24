6e9c418abd4925bb6aa4b632ceef263610663a07
gpuuser <svitrndvasad@gamil.com>
2023-02-24 10:04:41 +0530

    prov form with datat table done

--- admission-proc/src/routes/+page.js
+++ admission-proc/src/routes/+page.js
@@ -12,16 +12,16 @@ export async function load({}) {
         return {error}
 
     let { data:dataTable, error:dt_err } = await supabase
-        .from('ProvFormInfo').select(`*,Course(college_id)`)
+        .from('ProvFormInfo').select(`*,Course(college_id,name,alias),Branch(name,alias)`)
     if(dt_err)
         if(dt_err instanceof Error)
             return {error:dt_err.message}
+
     return {    
         aYearList,
         dataTable,
         collegeList
     };      
 }
-
 
 
