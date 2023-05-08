612ecc680a7eb4defd59549c65975c78673353df
gpuuser <svitrndvasad@gamil.com>
2023-05-08 11:12:47 +0530

    password reset

---
+++ admission-proc/src/routes/auth/resetauth/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    let password=''
+
+    const resetPwd=async()=>{
+        const { data, error } = await supabase.auth.updateUser({
+            password: password
+        })
+        if(error){
+            alert(error)
+        }
+    }
+</script>
+
+<div>
+    <h1 class="text-2xl text-slate-800">Reset Password</h1>
+    <form on:submit|preventDefault={resetPwd}>
+        <input type="password" bind:value={password} required>
+        <button type="submit">Reset</button>
+    </form>
+</div>
+
+
+
+
