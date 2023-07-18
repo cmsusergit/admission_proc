07355ba86b790c1b1d03b859fd729d6ee8cab845
gpuuser <svitrndvasad@gamil.com>
2023-07-18 10:42:50 +0530

    mqnri supabase error handled

--- admission-proc/src/lib/db.js
+++ admission-proc/src/lib/db.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/auth-helpers-sveltekit'
 import { env } from '$env/dynamic/public'
 
 console.log('****')
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL1, env.PUBLIC_SUPABASE_ANON_KEY1)
+export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
 
 // export const supabase_admin = createClient(env.PUBLIC_SUPABASE_URL, env.ADMIN_ACCESS_CREDENTIAL, {
 //     auth: {
