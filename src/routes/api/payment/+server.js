1f406cb7f70a6c3de5ee0ea93628a14b7538adb0
gpuuser <svitrndvasad@gamil.com>
2024-04-09 11:44:56 +0530

    payu test

--- src/routes/api/payment/+server.js
+++ src/routes/api/payment/+server.js
@@ -1,11 +1,20 @@
 export async function POST({ url,request }) {
 	const dt=await request.text()
 
-	console.log('****',dt);
+	const record1=JSON.parse(dt)
+	console.log(record1.customerPhone,record1.customerEmail)
+	
+	if(record1.status=='Success'){
+		const { data, error } = await supabase.from('MQNRIFormInfo')
+		.update({ is_payment_done: true })
+		.eq('contact', record1.customerPhone)
+		.select()
+		console.log('****',data);
+
+		if(error){
+			console.log('****',error);
+		}
+	}
+
 	return new Response(dt);
 }
-
-
-
-
-
