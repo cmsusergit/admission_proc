6f230614a60c3dec76115d5c0728cf0d52fc7a83
gpuuser <svitrndvasad@gamil.com>
2024-04-12 15:24:33 +0530

    test payu with env1

--- src/routes/api/payment/+server.js
+++ src/routes/api/payment/+server.js
@@ -1,10 +1,11 @@
+import { env } from '$env/dynamic/public'
 import { supabase } from '$lib/db'
-export async function POST({ url,request }) {
 
+export async function POST({ url,request }) {
 	const dt=await request.text()
 	const record1=JSON.parse(dt)
 	console.log(record1)
-	console.log(record1.status,record1.customerPhone);
+	console.log(record1.status,env);
 	if(record1.status=='Success'){
 		const { data1, error1 } = await supabase
 		.from('MQNRIFormInfo')
