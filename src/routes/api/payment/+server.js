9f7953e0acd76daab0d6ff88106f202e30a53e55
gpuuser <svitrndvasad@gamil.com>
2024-04-12 15:27:11 +0530

    payu test without env1

--- src/routes/api/payment/+server.js
+++ src/routes/api/payment/+server.js
@@ -1,22 +1,21 @@
-import { env } from '$env/dynamic/public'
 import { supabase } from '$lib/db'
-
 export async function POST({ url,request }) {
+
 	const dt=await request.text()
 	const record1=JSON.parse(dt)
 	console.log(record1)
-	console.log(record1.status,env);
 	if(record1.status=='Success'){
 		const { data1, error1 } = await supabase
 		.from('MQNRIFormInfo')
-		.select("*").like('contact', `'%${record1.customerPhone}%'`)
+		.select("*").like('email', `'%${record1.customerEmail}%'`)
 		console.log('dt::',data1)
-
+	
+	
 		if(error1)
 			console.log('error1::',error1)
 		const { data, error } = await supabase.from('MQNRIFormInfo')
 		.update({ is_payment_done: true })
-		.like('contact',`'%${record1.customerPhone}%'`)
+		.like('email',`'%${record1.customerEmail}%'`)
 		.select()
 		console.log('****',data);
 		if(error){
