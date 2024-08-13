846b792a0ff4cfc2f80eb15615120b1c2f8dbbb4
gpuuser <svitrndvasad@gamil.com>
2024-08-13 12:09:04 +0530

    mqnri change with number done

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,13 +4,14 @@
     import { college } from '$lib/store.js' 
     import { onMount } from "svelte";
     import Profile from '$lib/component/profile/mqnri_profile.svelte'
+    import _ from 'lodash'
     export let profileEmail
     let profile,uploadFileList
     onMount(async()=>{
         let { data:formInfo, error:formErr } = await supabase.from('MQNRIFormInfo')
             .select(`*,Course(*),College(*),AdmissionDocumentMQNRI(*,document_label(*))`).ilike('email',profileEmail).eq('is_removed',false)
-            if(formErr){
 
+            if(formErr){
                 console.log('****',formErr);
                 return {error:formErr.message}        
             }
@@ -18,18 +19,34 @@
                 college.set(formInfo[0].College)
                 profile=JSON.parse(JSON.stringify(formInfo[0]))
                 let merit_number=[]
-                if(profile.admission_category=='M' || profile.admission_category=='B'){
-                    let { data:meritdt1, error:err1_1 } = await supabase.from('mq_meritnumber').select(`*`).eq('id',profile.id).single()
-                    if(err1_1)
-                        return {error:err1_1.message}   
-                    merit_number.push({category:'M',number:meritdt1.merit_number})
+                if(profile.college_id==5){
+                    if(profile.admission_category=='M' || profile.admission_category=='B'){
+                        let { data:meritdt1, error:err1_1 } = await supabase.from('MQNRIFormInfo').select(`*`).eq('college_id',5)
+                        if(err1_1)
+                            return {error:err1_1.message}   
+                        let merit_number1
+                        const ordered1=_.orderBy(meritdt1,ob=>ob.total_merit,['desc'])
+                        ordered1.forEach((ob,indx) => {
+                            if(ob.id==profile.id)
+                                merit_number1=indx+1
+                        });
+                        merit_number.push({category:'M',college_id:5,number:merit_number1})                    
+                    }
+                }   
+                else{
+                    if(profile.admission_category=='M' || profile.admission_category=='B'){
+                        let { data:meritdt1, error:err1_1 } = await supabase.from('mq_meritnumber').select(`*`).eq('id',profile.id).single()
+                        if(err1_1)
+                            return {error:err1_1.message}   
+                        merit_number.push({category:'M',number:meritdt1.merit_number})
+                    }
+                    if(profile.admission_category=='N' || profile.admission_category=='B'){
+                        let { data:meritdt1, error:err1_1 } = await supabase.from('nri_meritnumber').select(`*`).eq('id',profile.id).single()
+                        if(err1_1)
+                            return {error:err1_1.message}       
+                        merit_number.push({category:'N',number:meritdt1.merit_number})
+                    }  
                 }
-                if(profile.admission_category=='N' || profile.admission_category=='B'){
-                    let { data:meritdt1, error:err1_1 } = await supabase.from('nri_meritnumber').select(`*`).eq('id',profile.id).single()
-                    if(err1_1)
-                        return {error:err1_1.message}       
-                    merit_number.push({category:'N',number:meritdt1.merit_number})
-                }  
                 //  
                 // let temp1=merit_number.find(ob=>ob.category=='M')
                 // if(temp1)
